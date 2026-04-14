d1241f31072f3323aa126aab869339651977c486
qiujiaqi <akikaede6@gmail.com>
2026-04-14 17:41:57 +0800

    6-7

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_ROW 5
 #define MAX_COL 5
+#define MAX_SIZE (MAX_ROW * MAX_COL)
 
 int maze[MAX_ROW][MAX_COL] = {
 	0, 1, 0, 0, 0,
@@ -11,10 +14,91 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+void print_path(Position path[], int len) {
+    for (int i = len - 1; i >= 0; i--) {
+        printf("(%d,%d)", path[i].x, path[i].y);
+		printf("\n");
+    }
+    printf("\n");
+}
+
+int is_valid(int x, int y) {
+    return x >= 0 && x < MAX_ROW &&
+           y >= 0 && y < MAX_COL &&
+           maze[x][y] == 0;
+}
+
+typedef struct {
+    int x;
+    int y;
+    int dir;   // 当前点已经尝试到第几个方向
+} StackNode;
+
+int solve_maze_stack(Position path[], int *path_len) {
+    StackNode stack[MAX_SIZE];
+    int top = -1;
+    int visited[MAX_ROW][MAX_COL] = {0};
+
+    /* 上、右、下、左 */
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    top++;
+    stack[top].x = 0;
+    stack[top].y = 0;
+    stack[top].dir = 0;
+    visited[0][0] = 1;
+
+    while (top >= 0) {
+        StackNode *cur = &stack[top];
+
+        if (cur->x == MAX_ROW - 1 && cur->y == MAX_COL - 1) {
+            *path_len = top + 1;
+            for (int i = 0; i <= top; i++) {
+                path[i].x = stack[i].x;
+                path[i].y = stack[i].y;
+            }
+            return 1;
+        }
+
+        if (cur->dir >= 4) {
+            top--;   // 四个方向都试完，回溯
+            continue;
+        }
+
+        int d = cur->dir;
+        cur->dir++;
+
+        int nx = cur->x + dx[d];
+        int ny = cur->y + dy[d];
+
+        if (is_valid(nx, ny) && !visited[nx][ny]) {
+            visited[nx][ny] = 1;
+            top++;
+            stack[top].x = nx;
+            stack[top].y = ny;
+            stack[top].dir = 0;
+        }
+    }
+
+    return 0;
+}
+
 int main(void)
 {
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    Position path[MAX_SIZE];
+    int len;
 
-	return 0;
+    if (solve_maze_stack(path, &len)) {
+        print_path(path, len);
+    } else {
+        printf("没有找到路径\n");
+    }
+
+    return 0;
 }
--- exercises/07_queue_maze/07_queue_maze.c
+++ exercises/07_queue_maze/07_queue_maze.c
@@ -2,6 +2,7 @@
 
 #define MAX_ROW 5
 #define MAX_COL 5
+#define MAX_SIZE MAX_ROW * MAX_COL
 
 int maze[MAX_ROW][MAX_COL] = {
 	0, 1, 0, 0, 0,
@@ -11,10 +12,127 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+/* =========================
+   打印路径
+   ========================= */
+void print_path(Position path[], int len) {
+    for (int i = len - 1; i >= 0; i--) {
+        printf("(%d,%d)", path[i].x, path[i].y);
+		printf("\n");
+    }
+    printf("\n");
+}
+
+int is_valid(int x, int y) {
+    return x >= 0 && x < MAX_ROW &&
+           y >= 0 && y < MAX_COL &&
+           maze[x][y] == 0;
+}
+
+typedef struct {
+    Position data[MAX_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void init_queue(Queue *q) {
+    q->front = 0;
+    q->rear = 0;
+}
+
+int queue_empty(Queue *q) {
+    return q->front == q->rear;
+}
+
+void enqueue(Queue *q, Position p) {
+    q->data[q->rear++] = p;
+}
+
+Position dequeue(Queue *q) {
+    return q->data[q->front++];
+}
+
+int solve_maze_queue(Position path[], int *path_len) {
+    Queue q;
+    init_queue(&q);
+
+    int visited[MAX_ROW][MAX_COL] = {0};
+    Position parent[MAX_ROW][MAX_COL];
+
+    for (int i = 0; i < MAX_ROW; i++) {
+        for (int j = 0; j < MAX_COL; j++) {
+            parent[i][j].x = -1;
+            parent[i][j].y = -1;
+        }
+    }
+
+    /* 上、右、下、左 */
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    Position start = {0, 0};
+    enqueue(&q, start);
+    visited[0][0] = 1;
+
+    while (!queue_empty(&q)) {
+        Position cur = dequeue(&q);
+
+        if (cur.x == MAX_ROW - 1 && cur.y == MAX_COL - 1) {
+            break;
+        }
+
+        for (int d = 0; d < 4; d++) {
+            int nx = cur.x + dx[d];
+            int ny = cur.y + dy[d];
+
+            if (is_valid(nx, ny) && !visited[nx][ny]) {
+                visited[nx][ny] = 1;
+                parent[nx][ny] = cur;
+
+                Position next = {nx, ny};
+                enqueue(&q, next);
+            }
+        }
+    }
+
+    if (!visited[MAX_ROW - 1][MAX_COL - 1]) {
+        return 0;
+    }
+
+    /* 逆向恢复路径 */
+    Position reverse_path[MAX_SIZE];
+    int cnt = 0;
+
+    Position cur = {MAX_ROW - 1, MAX_COL - 1};
+    while (!(cur.x == -1 && cur.y == -1)) {
+        reverse_path[cnt++] = cur;
+        cur = parent[cur.x][cur.y];
+    }
+
+    /* 反转 */
+    *path_len = cnt;
+    for (int i = 0; i < cnt; i++) {
+        path[i] = reverse_path[cnt - 1 - i];
+    }
+
+    return 1;
+}
+
 int main(void)
 {
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	Position path[MAX_SIZE];
+    int len;
+
+    if (solve_maze_queue(path, &len)) {
+        print_path(path, len);
+    } else {
+        printf("没有找到路径\n");
+    }
 
 	return 0;
 }
